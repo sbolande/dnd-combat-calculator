11e7e02933d4101a2151261b4e5cfc49a8574d53
Seth Bolander <bol15015@byui.edu>
2019-03-20 00:38:37 -0600

    second commit

--- main.js
+++ main.js
@@ -1,32 +1,21 @@
-const fs = require('fs');
-
 /* eslint-disable no-unused-vars */
 function toggleSidebar() {
     document.getElementById('sidebar').classList.toggle('active');
 }
 
-// Edit PLAYER.JSON
-function createPlayer() {
-
-}
-
-function removePlayer() {
-
-}
-
 // Add and subtract player
 function addPlayer() {
     var count = document.getElementById('count');
-    if (Number(count.innerHTML) < 9) {
-        count.innerHTML = `${Number(count.innerHTML) + 1}`;
-        createPlayer();
+    var num = Number(count.innerHTML);
+    if (num < 9) {
+        count.innerHTML = `${++num}`;
     }
 }
 
 function subtractPlayer() {
     var count = document.getElementById('count');
-    if (Number(count.innerHTML) > 0) {
-        count.innerHTML = `${Number(count.innerHTML) - 1}`;
-        removePlayer();
+    var num = Number(count.innerHTML);
+    if (num > 0) {
+        count.innerHTML = `${--num}`;
     }
 }
